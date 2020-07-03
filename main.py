ff62f3d6a3fab6c2aa63e03ed310c1c6822ac4eb
Yongkang Li <51050114+SocraLee@users.noreply.github.com>
2020-07-03 18:53:15 +0800

    增加了几个bgm，模拟了一个开场和一个结束
    稍微有点意思了？

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+# -*- coding: utf-8 -*-
 import pgzrun
 import time
-
+import wave
+import pygame#仅用来放音乐！！！
 WIDTH = 1280
 HEIGHT = 720
 
+pygame.mixer.init()
+bgmflag=True
+track=pygame.mixer.music.load(".\sounds\startbgm.mp3")
 eps = 1e-6
 
 player1_key = {
@@ -37,14 +42,14 @@ button = {
     'battle': {
     },
     'battle_end': {
-        'Back': Button(((WIDTH / 2 - 30, HEIGHT * 2 / 3 - 15), (60, 30)), (255, 0, 0), 'Back'),
+        'Back': Button(((WIDTH - 60, HEIGHT - 35), (60, 30)), (254, 67, 107), 'Back'),
     },
 }
 
 now_pressed_key = set()
 now_pressed_button = None
 
-now_page = 'battle'
+now_page = 'start'
 
 class Player(object):
 
@@ -53,7 +58,7 @@ class Player(object):
         self.type = type
         self.atk = 10
         self.hp = 100
-        self.balance=100
+        self.balance=0
         if type == 'player':
             self.actor = Actor('player1_block')
             self.actor.bottomleft = (0, HEIGHT)
@@ -111,31 +116,40 @@ player1 = Player('player')
 player2 = Player('enemy')
 
 def draw():
+    global bgmflag
     if now_page == 'start':
         screen.clear()
-        screen.fill((0, 0, 0))
+        screen.blit("start",(0,0))
+        if(bgmflag):
+            pygame.mixer.music.play()
+            bgmflag=False
 
     if now_page == 'setting':
         screen.clear()
         screen.fill((0, 0, 0))
 
     if now_page == 'battle':
+        if(bgmflag):
+            pygame.mixer.music.stop()
+            pygame.mixer.music.load(".\sounds\isschinbgm.mp3")
+            pygame.mixer.music.play()
+            bgmflag=False
         screen.clear()
-        screen.fill((255,255,255))
+        screen.fill((255,255,255))#白色背景，适用于黑色火柴人
         #玩家血条
         #screen.draw.filled_rect(Rect((25, 15), (500, 50)), (255, 255, 255))
         if player1.hp >= eps:
-            screen.draw.filled_rect(Rect((25, 15), (500 * player1.hp / 100, 50)), (255, 0, 0))
-            screen.draw.filled_rect(Rect((25, 65), (500 * player1.balance / 100, 30)), (255, 255, 0))
+            screen.draw.filled_rect(Rect((25, 15), (500 * player1.hp / 100, 50)), (254, 67, 60))
+            screen.draw.filled_rect(Rect((25, 65), (40+460 * player1.balance / 100, 30)), (255, 255-55*player1.balance/100, 20))
         screen.draw.text(str(player1.hp), (30, 20))
         screen.draw.text(str(player1.balance), (30, 70),color='black')
         #敌人血条
         #screen.draw.filled_rect(Rect((1280 - 525, 15), (500, 50)), (255, 255, 255))
         if player2.hp >= eps:
-            screen.draw.filled_rect(Rect((1280 - 25 - 500 * player2.hp / 100, 15), (500 * player2.hp / 100, 50)), (255, 0, 0))
-            screen.draw.filled_rect(Rect((1280 - 25 - 500 * player2.balance / 100, 65), (500 * player2.balance / 100, 30)), (255, 255, 0))
+            screen.draw.filled_rect(Rect((1280 - 25 - 500 * player2.hp / 100, 15), (500 * player2.hp / 100, 50)), (254, 67, 60))
+            screen.draw.filled_rect(Rect((1280 - 25 - 460 * player2.balance / 100-40, 65), (40+460 * player2.balance / 100, 30)), (255, 255-55*player1.balance/100, 20))
         screen.draw.text(str(player2.hp), (1280 - 60, 20))
-        screen.draw.text(str(player1.balance), (1280-60, 70),color='black')
+        screen.draw.text(str(player2.balance), (1280-60, 70),color='black')
 
         player1.actor.draw()
         player2.actor.draw()
@@ -146,7 +160,8 @@ def draw():
         if player2.hp < eps:
             screen.draw.text('player1 win', (WIDTH / 2, HEIGHT / 2))
         else:
-            screen.draw.text('菜', (WIDTH / 2, HEIGHT / 2))
+            screen.blit("failed",(0,0))
+
     
     for i, j in button[now_page].items():
         j.paint()
@@ -154,6 +169,7 @@ def draw():
 def attack(u, v):
     if u.actor.colliderect(v.actor):
         v.hp = max(v.hp - u.atk, 0)
+        v.balance=min(v.balance+u.atk,100)
         u.atk_last = time.time()
 
 def attack_defended(u, v):
@@ -194,9 +210,15 @@ def on_mouse_down(pos):
             now_pressed_button = i
 
 def on_key_down(key):
+    global now_page,bgmflag
+    if now_page=="start":
+        now_page="battle"
+        bgmflag=True
+        return
     now_pressed_key.add(key)
 
 def on_key_up(key):
-    now_pressed_key.remove(key)
+    if bgmflag==False:
+        now_pressed_key.remove(key)
 
 pgzrun.go()
